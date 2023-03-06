35e80719702a90e632ea4f9a0c2718d8b46101be
JD Grillo <jdgrillo@microsoft.com>
2023-03-06 13:52:42 -0800

    removing test

--- test_helpers/test_create_joke.py
+++ test_helpers/test_create_joke.py
@@ -9,5 +9,5 @@ def test_create_joke1():
     assert create_joke("jd", "Software") == "Rockstar" + " " + "jd"
 
 
-def test_create_joke2():
-    assert create_joke("jd", "ceo") == "Chief Happiness Officer" + " " + "jd"
+# def test_create_joke2():
+#     assert create_joke("jd", "ceo") == "Chief Happiness Officer" + " " + "jd"
