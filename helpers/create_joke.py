6e63a358adad06b494a17e0b99a6bc7270d06f4a
JDGrillo <jdgrillo@utexas.edu>
2023-03-07 22:01:17 +0000

    second error

--- helpers/create_joke.py
+++ helpers/create_joke.py
@@ -3,5 +3,6 @@ from helpers.joke_dict import joke_dict
 
 
 def create_joke(name, title):
+    convTitle = ""
     converted_title = convert_title(title)
     return joke_dict[converted_title] + " " + name
